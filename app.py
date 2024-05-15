f5f0fb35b986255765658ffe0274984a904a326e
PiCabss <131630239+PiCabss@users.noreply.github.com>
2024-05-15 16:45:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image
 import numpy as np
+
 # Load the trained model
 @st.cache(allow_output_mutation=True)
 def load_model():
@@ -15,10 +16,12 @@ def preprocess_image(image):
     img_array = img_array / 255.0  # Normalize pixel values
     img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
     return img_array
+
 # Function to make predictions
 def make_prediction(model, img_array):
     predictions = model.predict(img_array)
     return predictions
+
 # Display the prediction result
 def display_prediction(predictions):
     class_labels = ['Apple', 'Banana', 'Beetroot', 'Bell Pepper', 'Cabbage', 'Capsicum', 'Carrot', 'Cauliflower', 'Chilli Pepper', 'Corn',
@@ -28,26 +31,34 @@ def display_prediction(predictions):
     predicted_class_index = np.argmax(predictions[0])
     predicted_class = class_labels[predicted_class_index]
     return predicted_class
+
 # Main function to run the Streamlit app
 def main():
     # Load the model
     model = load_model()
+
     # Set up the Streamlit app layout
     st.title("Fruits and Vegetables Classifier")
     st.write("Upload an image of a fruit or vegetable to classify its species.")
+
     # File uploader for image input
     uploaded_file = st.file_uploader("Choose a fruit or vegetable image...", type=["jpg", "jpeg", "png"])
+
     if uploaded_file is not None:
         # Display the uploaded image
         image = Image.open(uploaded_file)
         st.image(image, caption="Uploaded Image", use_column_width=True)
+
         # Preprocess the image
         img_array = preprocess_image(image)
+
         # Make predictions
         predictions = make_prediction(model, img_array)
+
         # Display the prediction result
         predicted_class = display_prediction(predictions)
         st.success(f"The predicted fruit or vegetable is: {predicted_class}")
+
 # Run the Streamlit app
 if __name__ == "__main__":
     main()
